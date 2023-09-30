7e9acb0f40cb081302c78456e28939b80882a8b9
smithville77 <volcom_stone664@hotmail.com>
2023-09-30 21:25:23 +1000

    save game

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,12 @@ const cors = require("cors");
 const app = express();
 const router = express.Router();
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.json());
@@ -32,63 +38,181 @@ database.once('connected', () => {
   console.log('Fitness Database Connected');
 });
 
-const corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: '*',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+// };
 
 app.use(cors(corsOptions));
 
 app.use('/users', userRoutes);
 app.use("/exercise", runRoutes);
 
-const fitbitClientId = process.env.FB_CLIENT_ID;
-const fitbitClientSecret = process.env.FB_CLIENT_SECRET;
+// const fitbitClientId = process.env.FB_CLIENT_ID;
+// const fitbitClientSecret = process.env.FB_CLIENT_SECRET;
+// const fitbitRedirectUri = 'http://localhost:3000/callback';
+// const fitbitScopes = 'activity heartrate profile';
+
+// app.get('/auth/fitbit', cors(corsOptions), (req, res, next) => {
+//   console.log('Reached /fitbit route');
+//   // Construct the Fitbit authorization URL
+//   const fitbitAuthUrl = 'https://www.fitbit.com/oauth2/authorize?' +
+//     `response_type=code&` +
+//     `client_id=${fitbitClientId}&` +
+//     `redirect_uri=${fitbitRedirectUri}&` +
+//     `scope=${fitbitScopes}`;
+
+//   // Redirect the user to Fitbit for authorization
+//   res.redirect(fitbitAuthUrl);
+// });
+
+// app.get('/auth/fitbit/callback', async (req, res) => {
+//   const { code } = req.query;
+
+//   try {
+//     const tokenRequestData = {
+//       client_id: fitbitClientId,
+//       grant_type: 'authorization_code',
+//       code: code,
+//       redirect_uri: fitbitRedirectUri,
+//     };
+
+//     const response = await axios.post('https://api.fitbit.com/oauth2/token', null, {
+//       params: tokenRequestData,
+//       headers: {
+//         Authorization: `Basic ${Buffer.from(`${fitbitClientId}:${fitbitClientSecret}`).toString('base64')}`,
+//       },
+//     });
+
+//     const accessToken = response.data.access_token;
+//     const refreshToken = response.data.refresh_token;
+
+//     res.redirect('/auth/fitbit/success');
+//   } catch (error) {
+//     console.error(error);
+//     res.redirect('/auth/fitbit/error');
+//   }
+// });
+
+// app.listen(3001, () => {
+//   console.log("Server started on PORT 3001");
+// });
+
+
+
+const port = 3001;
+
+app.use(express.json());
+
+
+
+const crypto = require('crypto');
+
+// Function to generate a random code verifier
+function generateCodeVerifier() {
+  const codeVerifierLength = 128; // Length of the code verifier (minimum 43 characters)
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
+  let codeVerifier = '';
+
+  for (let i = 0; i < codeVerifierLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    codeVerifier += characters.charAt(randomIndex);
+  }
+
+  return codeVerifier;
+}
+
+// Function to generate the code challenge from the code verifier
+function generateCodeChallenge(codeVerifier) {
+  const codeChallenge = base64URLEncode(sha256(codeVerifier));
+  return codeChallenge;
+}
+
+// Helper function to calculate the SHA-256 hash of a string
+function sha256(input) {
+  return crypto.createHash('sha256').update(input).digest();
+}
+
+// Helper function to convert a buffer to base64 URL-safe format
+function base64URLEncode(buffer) {
+  return buffer.toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '');
+}
+
+module.exports = {
+  generateCodeVerifier,
+  generateCodeChallenge,
+};
+
+
+const fitbitClientId = '23RF4V';
+const fitbitClientSecret = '73a73bf8526e1bea2a0189cf6c3b674f';
 const fitbitRedirectUri = 'http://localhost:3000/callback';
 const fitbitScopes = 'activity heartrate profile';
 
-app.get('/auth/fitbit', cors(corsOptions), (req, res, next) => {
-  console.log('Reached /fitbit route');
-  // Construct the Fitbit authorization URL
-  const fitbitAuthUrl = 'https://www.fitbit.com/oauth2/authorize?' +
-    `response_type=code&` +
-    `client_id=${fitbitClientId}&` +
-    `redirect_uri=${fitbitRedirectUri}&` +
-    `scope=${fitbitScopes}`;
+app.get('/auth/fitbit', cors(corsOptions), async (req, res, next) => {
+  try {
+    // Generate PKCE values (codeVerifier and codeChallenge)
+    const codeVerifier = generateCodeVerifier();
+    const codeChallenge = generateCodeChallenge(codeVerifier);
 
-  // Redirect the user to Fitbit for authorization
-  res.redirect(fitbitAuthUrl);
+    // Create authorization URL with code challenge
+    const authUrl = `https://www.fitbit.com/oauth2/authorize?` +
+      `response_type=code&` +
+      `client_id=${fitbitClientId}&` +
+      `redirect_uri=${fitbitRedirectUri}&` +
+      `scope=${fitbitScopes}&` +
+      `code_challenge=${codeChallenge}&` +
+      `code_challenge_method=S256`;
+
+    res.json({ authorizationUrl: authUrl });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
-app.get('/auth/fitbit/callback', async (req, res) => {
-  const { code } = req.query;
+app.get('/callback', (req, res) => {
+  const { code, state } = req.query;
+  
+  if (state !== expectedState) {
+    return res.status(400).json({ error: 'Invalid state parameter' });
+  }
 
   try {
-    const tokenRequestData = {
-      client_id: fitbitClientId,
-      grant_type: 'authorization_code',
-      code: code,
-      redirect_uri: fitbitRedirectUri,
-    };
-
-    const response = await axios.post('https://api.fitbit.com/oauth2/token', null, {
-      params: tokenRequestData,
+    // Exchange the authorization code for tokens
+    const response = axios.post('https://api.fitbit.com/oauth2/token', null, {
+      params: {
+        client_id: fitbitClientId,
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: fitbitRedirectUri,
+      },
       headers: {
         Authorization: `Basic ${Buffer.from(`${fitbitClientId}:${fitbitClientSecret}`).toString('base64')}`,
       },
     });
 
+    // Extract access token and optionally refresh token from the response
     const accessToken = response.data.access_token;
     const refreshToken = response.data.refresh_token;
 
-    res.redirect('/auth/fitbit/success');
+    // Store the tokens securely, e.g., in a database or session
+
+    // Redirect the user to a success page or perform further actions
+    res.redirect('/success');
   } catch (error) {
     console.error(error);
-    res.redirect('/auth/fitbit/error');
+    res.redirect('/error'); // Redirect to an error page if there's an issue
   }
 });
 
-app.listen(3001, () => {
-  console.log("Server started on PORT 3001");
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
+
--- client/src/components/FitBitAuthBtn.tsx
+++ client/src/components/FitBitAuthBtn.tsx
@@ -1,28 +1,50 @@
-import React from 'react';
-import axios from 'axios'; 
-axios.defaults.baseURL = 'http://localhost:3001';
+// import React from 'react';
+// import axios from 'axios'; 
+// axios.defaults.baseURL = 'http://localhost:3001';
 
 
+
+// const FitbitAuthButton = () => {
+//   const handleFitbitAuth = async () => {
+//     try {
+//       console.log('Sending request to /auth/fitbit'); 
+//       // Make an API request to your backend to initiate the Fitbit OAuth2 flow
+//       await axios.get('/auth/fitbit');
+//       // You can also handle the response if needed
+      
+//     } catch (error) {
+//       console.error(error);
+//       // Handle any errors that occur during the API request
+//     }
+//   };
+
+//   return (
+//     <button onClick={handleFitbitAuth}>Authorize with Fitbit</button>
+//   );
+// };
+
+// export default FitbitAuthButton;
+
+
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const FitbitAuthButton = () => {
-  const handleFitbitAuth = async () => {
+  const [authorizationUrl, setAuthorizationUrl] = useState('');
+
+  const initiateFitbitAuth = async () => {
     try {
-      console.log('Sending request to /auth/fitbit'); 
-      // Make an API request to your backend to initiate the Fitbit OAuth2 flow
-      await axios.get('/auth/fitbit');
-      // You can also handle the response if needed
-      
+      const response = await axios.get('http://localhost:3001/auth/fitbit');
+      const { authorizationUrl } = response.data;
+      window.location.href = authorizationUrl; // Redirect the user to Fitbit
     } catch (error) {
       console.error(error);
-      // Handle any errors that occur during the API request
     }
   };
 
   return (
-    <button onClick={handleFitbitAuth}>Authorize with Fitbit</button>
+    <button onClick={initiateFitbitAuth}>Authorize with Fitbit</button>
   );
 };
 
 export default FitbitAuthButton;
-
-
