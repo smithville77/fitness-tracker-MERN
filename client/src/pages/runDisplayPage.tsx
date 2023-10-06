e483193e6531de4ac903e9eb9a078a656ae47475
smithville77 <volcom_stone664@hotmail.com>
2023-10-07 10:50:35 +1100

    added run filtering by range

--- client/src/pages/runDisplayPage.tsx
+++ client/src/pages/runDisplayPage.tsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 function RunDisplayPage() {
   const [runData, setRunData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedRange, setSelectedRange] = useState(null);
 
   useEffect(() => {
     // Retrieve the access token from localStorage
@@ -28,23 +29,62 @@ function RunDisplayPage() {
       });
   }, []);
 
+  const ranges = [
+    { label: 'All Ranges', min: 0, max: 1000 }, 
+    { label: '0 - 4.9 km', min: 0, max: 4.9 },
+    { label: '5 - 9.9 km', min: 5, max: 9.9 },
+    { label: '10 - 14.9 km', min: 10, max: 14.9 },
+    { label: 'greater than 15 km', min: 15, max: 1000 },
+  ];
+
+  // Function to filter data based on the selected range
+  const filterData = () => {
+    if (!selectedRange) return runData; // Return all data if no range is selected
+
+    const filteredData = runData.filter((item) => {
+      const distance = item.distance;
+      return distance >= selectedRange.min && distance <= selectedRange.max;
+    });
+
+    return filteredData;
+  };
+
+  const handleRangeChange = (e) => {
+    const selectedIndex = e.target.selectedIndex;
+    const selectedRange = ranges[selectedIndex];
+    setSelectedRange(selectedRange);
+  };
+
+  const filteredData = filterData();
+
   return (
     <div className="Profile">
       <Navigation />
-      {loading ? (
-        <p>Loading profile data...</p>
-      ) : runData && runData.length > 0 ? (
-        <div>
-          {runData.map((item, index) => (
-            <div key={index}>
-              <p>Run distance: {item.distance}</p>
-              {/* You can display other information here */}
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>No run data available</p>
-      )}
+      <p>Recent runs total: {runData.length}</p>
+      <p>Select runs by distance</p>
+      <select id="range-dropdown" onChange={handleRangeChange}>
+        {ranges.map((range, index) => (
+          <option key={index} value={index}>
+            {range.label}
+          </option>
+        ))}
+      </select>
+      <div>
+        {loading ? (
+          <p>Loading run data...</p>
+        ) : filteredData.length > 0 ? (
+          <div>
+            {filteredData.map((item, index) => (
+              <div key={index}>
+                <p>Run distance: {item.distance} Run speed: {parseFloat(item.speed).toFixed(2)}</p>
+                
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>No run data available for the selected range</p>
+        )}
+      </div>
     </div>
   );
 }
