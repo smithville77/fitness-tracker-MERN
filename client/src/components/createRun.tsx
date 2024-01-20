737ddb9268ece5ad27433f0cdfea9072c29f8380
smithville77 <volcom_stone664@hotmail.com>
2024-01-20 18:29:31 +1100

    code cleanup

--- client/src/components/createRun.tsx
+++ client/src/components/createRun.tsx
@@ -1,42 +1,38 @@
-import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import axios from 'axios';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 function CreateRunForm() {
   // Define state variables for distance and duration
-  const [distance, setDistance] = useState('');
-  const [duration, setDuration] = useState('');
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
 
-  
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     try {
-      // Retrieve the token from local storage
-      const token = localStorage.getItem('token');
-  
-      // Make a POST request with the user input and token in the headers
+      const token = localStorage.getItem("token");
+
       const response = await axios.post(
-        'http://localhost:3001/exercise/newrun',
+        "http://localhost:3001/exercise/newrun",
         {
           distance: parseFloat(distance),
           duration: parseInt(duration),
         },
         {
           headers: {
-            Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+            Authorization: `Bearer ${token}`,
           },
         }
       );
-  
+
       if (response.status === 200) {
         console.log(response.status);
-        setDistance('');
-        setDuration('');
+        setDistance("");
+        setDuration("");
       }
     } catch (error) {
       console.error("Error:", error);
-      // Handle errors here if needed
     }
   };
 
