8afb6617182d5deea6039c1503085a34c4d1209d
smithville77 <volcom_stone664@hotmail.com>
2024-01-27 18:14:46 +1100

    styling changes

--- client/src/components/ScrollTop.tsx
+++ client/src/components/ScrollTop.tsx
@@ -11,7 +11,7 @@ function ScrollTop() {
   return (
     <button
       id="scroll-top"
-      className="fixed bottom-20 right-10 h-10 w-10 rounded-full bg-orange-500 text-2xl flex justify-center items-center"
+      className="fixed bottom-20 right-10 h-10 w-10 rounded-full text-2xl flex justify-center items-center"
       onClick={scrollToTop}
     >
       <p className="flex justify-center items-center mt-2">⌃</p>
